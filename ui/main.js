293e6be688c13ae0d9b622ad9ab90ed0b7ac23e7
mayanksagar26 <mayanksagar26@outlook.com>
2016-10-13 16:19:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,9 @@
 console.log('Loaded!');
 
+//Change the text of the inner Html file by adding a id
 var element = document.getElementById('main-text');
 
+//Calling the inner HTML of the html file and changing rhe content
 element.innerHTML = 'Click on the dinasour to see it moving';
 
 //Move the Image of the Dinasour
