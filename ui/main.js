ddeac62f102f84da35b743ba4d923bce6279c71d
mayanksagar26 <mayanksagar26@outlook.com>
2016-10-13 16:05:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,6 @@ element.innerHTML = 'Click on the dinasour to see it moving';
 //Move the Image of the Dinasour
  
 var img = document.getElementById('madi');
-
 img.onclick = function () {
     img.style.marginLeft = '100px';
 };
