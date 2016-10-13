f8a8ea22478038e66be839474c6b465602730dbe
mayanksagar26 <mayanksagar26@outlook.com>
2016-10-13 15:40:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 console.log('Loaded!');
 
-var element = document.getElementById('main.text');
+var element = document.getElementById('main-text');
 
 element.innerHTML = 'Click on the dinasour to see it moving';
