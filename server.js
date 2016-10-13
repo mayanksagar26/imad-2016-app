418e2de9efb1bf99cfe9c9409caa765b19a350bd
mayanksagar26 <mayanksagar26@outlook.com>
2016-10-13 14:39:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,7 +79,6 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName', function (req, res){
-    //aticle name
     var articleName = req.params.articleName;
       res.send(createTemplate(articles[articleName]));
     });
